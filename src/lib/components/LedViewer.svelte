792ac5359913787fa2dae5fb23f71164b2b3eba3
Terence Dumas <dumast@awty.org>
2023-03-22 14:17:45 -0500

    I just left ramp hell

--- src/lib/components/LedViewer.svelte
+++ src/lib/components/LedViewer.svelte
@@ -8,7 +8,6 @@
 
     function doSetTimeout(i, timeWait, isRamp) {
         setTimeout(function() {
-            console.log(z)
             if(isRamp){
                 z = i
             } else {
@@ -17,38 +16,41 @@
         }, timeWait);
     }
 
-    function doSetTimeout2(i, timeWait, timeSum) {
+    function doSetTimeout2(i, timeSum) {
         setTimeout(function() {
-            for(let i2 = 0; i2 < (data[i].duration) * 50; i2++) {
-                console.log("DATA", 1/(data[i].duration * (50)) * (i2+1))
-                doSetTimeout(1/(data[i].duration * (50)) * (i2+1), data[i].duration*1000 / 50 + timeSum, true) //!
-                timeSum = timeSum/1.0 + data[i].duration*1000 / 50
+            let localTimeSum = 0
+            y = i;
+            for(let j = 0; j < (data[i].duration) * 50; j++) {
+                doSetTimeout(1/(data[i].duration * 50) * (j+1), data[i].duration*1000 / 50 + localTimeSum, true) //!
+                localTimeSum = localTimeSum/1.0 + 1000 / 50;
             }
-        }, timeWait)
-        y = i;
+        }, timeSum)
     }
 
     function playAnimation(){
         y = 0;
         z = 0;
         timeSum = 0
-        for(let i = 1; i < data.length; i++) {
+        for(let i = 0; i < data.length; i++) {
             if(data[i].type == "static") {
-                doSetTimeout(i, data[i-1].duration*1000 + timeSum, false);
-                timeSum = timeSum/1.0 + data[i-1].duration*1000;
+                doSetTimeout(i, timeSum, false);
+                timeSum = timeSum/1.0 + data[i].duration*1000;
             } else {
-                doSetTimeout2(i, data[i-1].duration*1000 + timeSum, timeSum)
+                doSetTimeout2(i, timeSum)
+                timeSum = timeSum/1.0 + data[i].duration*1000;
                 z = 0;
             }
-        }
     }
+}
+
 </script>
 
+<!-- svelte-ignore non-top-level-reactive-declaration -->
 <div>
     <button on:click={() => playAnimation()}>Play</button>
     <div class = "Viewer">
         <!-- <div style="background-color: {data[y].color}" class = "Percent"></div> -->
-        <div style = "background: linear-gradient(90deg, {data[y].color} 0%, {data[y].color} {data[y].length}%, rgba(0,0,0,1) {data[y].type == "ramp" ? data[y].length*z : data[y].length}%); box-shadow: inset 0 0 12px 12px #2D2E39, inset 0 0 3px 2px #2D2E39;" class = "Percent"></div>
+        <div style = "background: linear-gradient(90deg, {data[y].color} 0%, {data[y].color} {data[y].type == "ramp" ? data[y].length * z: data[y].length}%, rgba(0,0,0,1) {data[y].type == "ramp" ? data[y].length * z : data[y].length}%); box-shadow: inset 0 0 12px 12px #2D2E39, inset 0 0 3px 2px #2D2E39;" class = "Percent"></div>
     </div>
 </div>
 
